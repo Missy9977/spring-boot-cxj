a5352cca284b9b71eef566ed1e41ef899e95d85e
楚先杰 <1138789533@qq.com>
2019-12-28 17:38:24 +0800

    【楚先杰2019年12月28日】初始化

---
+++ src/main/java/com/cxj/springbootcxj/SpringBootCxjApplication.java
@@ -0,0 +1,13 @@
+package com.cxj.springbootcxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCxjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCxjApplication.class, args);
+    }
+
+}
